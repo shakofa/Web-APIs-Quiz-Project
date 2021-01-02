3c0e467f83b6067220cb7e4ca94a31e22f87e323
Anahita <shakofa.mahmody@gmail.com>
2021-01-01 18:16:57 -0800

    Added an image to index.html file. developed style sheet and added style for html files.

---
+++ js/logic.js
@@ -0,0 +1,14 @@
+// variables to refrence Dom elements
+var timerEl =document.getElementById("time");
+var timerEl =document.getElementById("startBtn");
+var timerEl =document.getElementById("questions");
+var timerEl =document.getElementById("choices");
+var timerEl =document.getElementById("submit");
+var timerEl =document.getElementById("initials");
+var timerEl =document.getElementById("feedback");
+
+
+//variables to keep track of quiz state
+var currentQuestionIndex = 0;
+var time = question.length * 15;
+var timerId; 
